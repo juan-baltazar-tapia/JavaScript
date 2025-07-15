42d22843e5739bb6c17f4cc2c129e7c89e9cf70b
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-14 23:01:53 -0400

    Completed Leap Years 2

--- leapYears.js
+++ leapYears.js
@@ -15,7 +15,7 @@ function isLeapYear(year){
         return false;
     }
 }
-console.log(isLeapYear(2016));      // true
+console.log(isLeapYear(2016));      // true.  
 console.log(isLeapYear(2015));      // false
 console.log(isLeapYear(2100));      // false
 console.log(isLeapYear(2400));      // true
@@ -27,4 +27,4 @@ console.log(isLeapYear(1752));      // true
 console.log(isLeapYear(1700));      // false
 console.log(isLeapYear(1));         // false
 console.log(isLeapYear(100));       // false
-console.log(isLeapYear(400)); // true
+console.log(isLeapYear(400));       // true

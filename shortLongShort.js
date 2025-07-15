02ef3633280e82cfe7c1ee55f66c67fcabb7a5ea
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-14 22:40:52 -0400

    Completed Short Long Short

---
+++ shortLongShort.js
@@ -0,0 +1,12 @@
+// Write a function that takes two strings as arguments, determines the length of 
+//the two strings, and then returns the result of concatenating the shorter string, the longer string, and the
+//  shorter string once again. You may assume that the strings are of different lengths.
+function shortLongShort(string1, string2){
+    if (string1.length < string2.length){
+        return string1 + string2 + string1
+    }
+    return string2 + string1 + string2
+}
+console.log(shortLongShort('abc', 'defgh'));    // "abcdefghabc"
+console.log(shortLongShort('abcde', 'fgh'));    // "fghabcdefgh"
+console.log(shortLongShort('', 'xyz'));

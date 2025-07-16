d95e8632994d0ceabeb2fbf56bcc46b96eafbf89
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 14:38:54 -0400

    Revised

--- ExclusiveOr.js
+++ ExclusiveOr.js
@@ -6,10 +6,6 @@
 // In this exercise, you will write a function named xor that takes two arguments, and returns true if exactly one of its arguments is
 //  truthy, false otherwise. Note that we are looking for a boolean result instead of a truthy/falsy value as returned by || and &&.
 function xor(valueOne, valueTwo){
-    //console.log(!(valueOne == valueTwo));
-    //console.log((valueOne || valueTwo) !== false);
-    // If x can be converted to true, returns x; else, returns y.
-    // Checks if the or operator returns false, else it is true
     return Boolean(valueOne) !== Boolean(valueTwo)
 }
 

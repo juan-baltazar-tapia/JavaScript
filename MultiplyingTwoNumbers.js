a3362fbdc35ffec30cb32a1a16923fb91bc424c2
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 14:01:53 -0400

    Completed Multiplying Two Numbers

---
+++ MultiplyingTwoNumbers.js
@@ -0,0 +1,5 @@
+//Create a function that takes two arguments, multiplies them together, and returns the result.
+function multiply(numberOne, numberTwo){
+    return numberOne * numberTwo;
+}
+console.log(multiply(5, 3) === 14); // logs true

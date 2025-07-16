1eef5288db1db65e13a063d406f23abb2b0cd7cd
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 15:24:53 -0400

    Revised Alwasy Return Negative

--- AlwaysReturnNegative.js
+++ AlwaysReturnNegative.js
@@ -1,7 +1,7 @@
 // Write a function that takes a number as an argument. If the argument is a positive number, 
 // return the negative of that number. If the argument is a negative number, return it as-is.
 function negative(number){
-    return number < 0 ? number : 0-number;
+    return number > 0 ? -number : number;
 }
 console.log(negative(5));     // -5
 console.log(negative(-3));    // -3

02582fdae01e158b7b65207efcc311a60db49391
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 13:45:17 -0400

    Revised Welcome Stranger with Claude AI Example

--- WelcomeStranger.js
+++ WelcomeStranger.js
@@ -1,15 +1,13 @@
 // Create a function that takes 2 arguments, an array and an object. The array will contain 2 or more 
 // elements that, when combined with adjoining spaces, will produce a person's name. The object will contain two keys,
-//  "title" and "occupation", and the appropriate values. Your function should return a greeting that uses the person's fu
-//  ll name, an
-// d mentions the person's title.
+//  "title" and "occupation", and the appropriate values. Your function should return a greeting that uses the person's 
+// full name, and mentions the person's title.
 
-function greetings(array, object){
-    let fullName = array[0];
-    for (let i = 1; i<array.length; i++){
-        fullName = fullName + " " + array[i]
-    }
-    return(`Hello ${fullName}! Nice to have a ${object.title} ${object.occupation} around.`)
+function greetings(nameArray, personInfoObject){
+    const fullName = nameArray.join(' ');
+    const title = personInfoObject.title;
+    const occupation = personInfoObject.occupation;
+    return(`Hello ${fullName}! Nice to have a ${title} ${occupation} around.`)
 }
 
 console.log(

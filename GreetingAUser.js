d71e97458c086a4d3132fce7777cfad060d659f6
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 13:51:30 -0400

    Completed Greeting A User V1

---
+++ GreetingAUser.js
@@ -0,0 +1,10 @@
+// Write a program that will ask for user's name. The program will then greet the user. If the
+//  user writes "name!" then the computer yells back to the user.
+const prompt = require('prompt-sync')();
+const userInput = prompt('Hello, what is your name?');
+if (userInput[userInput.length-1] === "!"){
+    const name = userInput.slice(0,userInput.length-1).toUpperCase();
+    console.log(`HELLO ${name}, WHY ARE WE SCREAMING?`)
+} else {
+    console.log(`Hello ${userInput}`)
+}

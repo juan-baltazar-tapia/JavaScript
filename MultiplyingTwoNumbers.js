409751d230b23a0776f53a5df2b21cba2072ac03
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 14:02:45 -0400

    Revised Multipling Two Numbers.

--- MultiplyingTwoNumbers.js
+++ MultiplyingTwoNumbers.js
@@ -1,5 +1,4 @@
 //Create a function that takes two arguments, multiplies them together, and returns the result.
-function multiply(numberOne, numberTwo){
-    return numberOne * numberTwo;
-}
-console.log(multiply(5, 3) === 14); // logs true
+
+const multiply = (a,b) => a*b;
+console.log(multiply(5, 3) === 15); // logs true

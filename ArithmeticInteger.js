d64c07fc8e71f0bb9e1a3afaaed094beed593eb1
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 14:09:43 -0400

    Completed Arithmetic Integer

---
+++ ArithmeticInteger.js
@@ -0,0 +1,25 @@
+// Write a program that prompts the user for two positive integers, and then prints the 
+// results of the following operations on those two
+//  numbers: addition, subtraction, product, quotient, remainder, and power.
+//  Do not worry about validating the input.
+
+const prompt = require('prompt-sync')();
+const firstNumber = Number(prompt('Enter the first number: '));
+const secondNumber = Number(prompt('Enter the second nymber: '));
+console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+console.log(`${firstNumber} / ${secondNumber} = ${Math.floor(firstNumber / secondNumber)}`);
+console.log(`${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
+console.log(`${firstNumber} ** ${secondNumber} = ${firstNumber ** secondNumber}`);
+
+// ==> Enter the first number:
+// 23
+// ==> Enter the second number:
+// 17
+// ==> 23 + 17 = 40
+// ==> 23 - 17 = 6
+// ==> 23 * 17 = 391
+// ==> 23 / 17 = 1
+// ==> 23 % 17 = 6
+// ==> 23 ** 17 = 1.4105003956066297e+23

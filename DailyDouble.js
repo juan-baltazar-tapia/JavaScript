f5fc977f0c07d75111dbe8bd2fd799c4c3fbc8e2
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 20:08:54 -0400

    Completeed Daily Double

---
+++ DailyDouble.js
@@ -0,0 +1,24 @@
+// Write a function that takes a string argument and returns a new string that conta
+// ins the value of the original string with all consecutive duplicate characters collapsed into a single character.
+
+function crunch(string){
+    let output = '';
+    //use two pointers
+    let leftPointer = 0;
+    let rightPointer = 1;
+    while (rightPointer <= string.length){
+        if (string.charAt(leftPointer) !== string.charAt(rightPointer)){
+        output = output + string.charAt(leftPointer)
+        }
+        leftPointer += 1;
+        rightPointer += 1;
+    }
+
+    return output
+}
+
+console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch('4444abcabccba'));              // "4abcabcba"
+console.log(crunch('ggggggggggggggg'));            // "g"
+console.log(crunch('a'));                          // "a"
+console.log(crunch(''));                           // ""

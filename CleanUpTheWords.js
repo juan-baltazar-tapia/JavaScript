74763e5d06009c9019598893f2288bb03f25d957
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-16 23:00:43 -0400

    Revised with AI, wtf shouldve known about .trim()

--- CleanUpTheWords.js
+++ CleanUpTheWords.js
@@ -4,17 +4,6 @@
 // the result (i.e., the result string should never have consecutive spaces).
 
 function cleanUp(str){
-    let result = '';
-    for (let i = 0; i < str.length; i++) {
-    const char = str[i]
-    //console.log(char)
-    let flag = true
-    if (/[a-zA-Z0-9]/.test(char)) {
-        result += char;
-    }
-    else {
-        result += ' '
-    }};
-    return result; 
+      return str.replace(/[^a-zA-Z]+/g, ' ').trim();
 }
 console.log(cleanUp("---what's my +*& line?"));    // " what s my line "

343759aeb954b6393c3d98268a5569ebaf159ace
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-17 22:05:53 -0400

    Completed Palindrome v2

--- palindrome.js
+++ palindrome.js
@@ -2,11 +2,13 @@
 //  A palindrome reads the same forwards and backwards. For this problem, the case matters and all characters matter.
 function isPalindrome(string){
     // split function and half, reverse second half
-    const midPoint = Math.floor(string.length / 2);
-    const firstHalf = string.slice(0,midPoint);
-    const secondHalf = string.length % 2 === 0 ? string.slice(midPoint) : string.slice(midPoint + 1);
-    const secondHalfReversed = secondHalf.split('').reverse().join('');
-    return firstHalf === secondHalfReversed;
+    // const midPoint = Math.floor(string.length / 2);
+    // const firstHalf = string.slice(0,midPoint);
+    // const secondHalf = string.length % 2 === 0 ? string.slice(midPoint) : string.slice(midPoint + 1);
+    // const secondHalfReversed = secondHalf.split('').reverse().join('');
+    // return firstHalf === secondHalfReversed;
+    const reverse = string.split('').reverse().join('');
+    return string === reverse;
 }
 console.log(isPalindrome('madam'));               // true
 console.log(isPalindrome('Madam'));               // false (case matters)

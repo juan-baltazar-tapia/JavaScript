c7109c021b53f913203873faec3ecc92c4a41c1f
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-17 22:04:50 -0400

    Completed Palindrome v1

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+// Write a function that returns true if the string passed as an argument is a palindrome, or false otherwise.
+//  A palindrome reads the same forwards and backwards. For this problem, the case matters and all characters matter.
+function isPalindrome(string){
+    // split function and half, reverse second half
+    const midPoint = Math.floor(string.length / 2);
+    const firstHalf = string.slice(0,midPoint);
+    const secondHalf = string.length % 2 === 0 ? string.slice(midPoint) : string.slice(midPoint + 1);
+    const secondHalfReversed = secondHalf.split('').reverse().join('');
+    return firstHalf === secondHalfReversed;
+}
+console.log(isPalindrome('madam'));               // true
+console.log(isPalindrome('Madam'));               // false (case matters)
+console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
+console.log(isPalindrome('356653'));              // true
+

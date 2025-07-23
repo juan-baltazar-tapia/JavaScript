71b9936793ca33c3e318925ed39dcb78876c514e
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-23 14:46:32 -0400

    Sum of Sums v2

--- SumOfSums.js
+++ SumOfSums.js
@@ -1,11 +1,13 @@
 // Write a function that takes an array of numbers and returns the sum of the sums of each leading subsequence in that array.
 //  Examine the examples to see what we mean. You may assume that the array always contains at least one number.
 function sumOfSums(array){
-    let sum = 0;
+    let finalSum = 0;
+    let currentSum = 0
     for (let i = 0; i < array.length; i++){
-        sum += array.slice(0,i+1).reduce((acc, curr)=> acc + curr);
+        currentSum += array[i];
+        finalSum += currentSum;
     }
-    return sum;
+    return finalSum;
 }
 console.log(sumOfSums([3, 5, 2]));        // (3)) + (3 + 5)) + (3 + 5 + 2)) --> 21
 console.log(sumOfSums([1, 5, 7, 3]));     // (1)) + (1 + 5)) + (1 + 5 + 7)) + (1 + 5 + 7 + 3)) --> 36

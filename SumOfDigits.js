3b2d112feca0f7848a7957fcbcc18083a65de5bf
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-23 11:11:13 -0400

    Completed Sum Of Digits

---
+++ SumOfDigits.js
@@ -0,0 +1,9 @@
+// Write a function that takes one argument, a positive integer, and returns the sum of its digits. 
+// Do this without using for, while, or do...while loops - instead, use a series of method calls to perform the sum.
+function sum(integer){
+    return integer.toString().split('').reduce((acc, sum)=> Number(acc) + Number(sum), 0);
+}
+
+console.log(sum(23));           // 5
+console.log(sum(496));          // 19
+console.log(sum(123456789));    // 45

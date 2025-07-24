840abb255a33eadd0ca9d3cbd1a2b9f15db8e18e
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-23 21:26:11 -0400

    Completed SearchWord v2

--- SearchWord.js
+++ SearchWord.js
@@ -6,15 +6,9 @@
 // Also assume that the search is case-insensitive.
 function searchWord(word, text){
     const lowerCaseWord = word.toLowerCase();
-    let sum = 0;
-    const textArray = text.split(' ')
-    for (const word of textArray){
-        if (word.toLowerCase() === lowerCaseWord){
-            sum += 1
-        }
-    }
-    return sum;
+    return result = text.split(' ').filter(word => word.toLowerCase() === lowerCaseWord).length    
 }
+
 
 const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
 

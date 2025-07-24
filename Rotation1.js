08088ba7a8fe2b92fd0f6ba9c1622f801c9d1848
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-24 19:30:15 -0400

    Completed Rotation2

---
+++ Rotation1.js
@@ -0,0 +1,33 @@
+// Write a function that rotates an array by moving the first element to the end of the array. Do not modify the original array.
+
+// If the input is not an array, return undefined.
+// If the input is an empty array, return an empty array.
+// Review the test cases below, then implement the solution accordingly.
+function rotateArray(array){
+    if (!Array.isArray(array)) return undefined;
+    if (array.length === 0) return [];
+    if (array.length === 1) return array;
+    const result = array.slice(1).concat(array.slice(0,1));
+    return result;
+}
+
+rotateArray([7, 3, 5, 2, 9, 1]);       // [3, 5, 2, 9, 1, 7]
+rotateArray(['a', 'b', 'c']);          // ["b", "c", "a"]
+rotateArray(['a']);                    // ["a"]
+rotateArray([1, 'a', 3, 'c']);         // ["a", 3, "c", 1]
+rotateArray([{ a: 2 }, [1, 2], 3]);    // [[1, 2], 3, { a: 2 }]
+console.log(rotateArray([]));                       // []
+
+// // return `undefined` if the argument is not an array 
+console.log(rotateArray());                         // undefined
+console.log(rotateArray(1));                        // undefined
+
+
+// the input array is not mutated
+let array = [1, 2, 3, 4];
+console.log(`Original array ${array}`)
+rotateArray(array);                    // [2, 3, 4, 1]
+console.log(`After calling method ${array}`)
+array;                                 // [1, 2, 3, 4]
+
+

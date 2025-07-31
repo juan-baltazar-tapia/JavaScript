cb0880c939c5bc4ac64a7057e134eed689165727
Juan Baltazar Tapia <juan.space14@gmail.com>
2025-07-30 20:58:02 -0400

    Completed Fib Num memo

---
+++ FibNumMemoization.js
@@ -0,0 +1,16 @@
+//Our recursive fibonacci function from an earlier exercise isn't very efficient. It starts slowing down with an nth argument value as low as 35. One way to improve the performance of our recursive fibonacci function (and other recursive functions) is to use memoization.
+
+// Memoization is an approach that involves saving a computed answer for future reuse, instead of computing it from scratch every time it is needed. In the case of our recursive fibonacci function, using memoization saves calls to fibonacci(nth - 2) because the necessary values have already been computed by the recursive calls to fibonacci(nth - 1).
+
+// For this exercise, your objective is to refactor the recursive fibonacci function to use memoization.
+function fibonacci(n, memo = {}){
+    if (n in memo) return memo[n];
+    if (n <= 1) return n;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
+   
+}
+
+console.log(fibonacci(20));       // 6765
+console.log(fibonacci(50));       // 12586269025
+console.log(fibonacci(75)); 
